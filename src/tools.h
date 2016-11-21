758a450e075abe5b00541cf7c98f04a35cee10c9
Francisco <fgonz@outlook.com>
2016-11-21 00:20:11 -0300

    documentacion

--- src/tools.h
+++ src/tools.h
@@ -1,14 +1,14 @@
 #ifndef TOOLS_H
 #define TOOLS_H
+#include <string>
 #include "gameEngine.h"
 // funcion que carga una malla desde filename
 
 bool load_mesh (const char* file_name, GLuint* vao, int* point_count);
 
-void instantiate_car();
-void instantiate_object();
+void load_player_vehicle();
+void load_object(string objectName);
 
 void input_keyb(gameEngine* ge);
-
 
 #endif 
